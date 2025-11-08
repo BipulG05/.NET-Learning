cc1af7ce82def2ba953808b5dd0f361e091c95d4
BipulG05 <bipulguchhait05@gmail.com>
2025-11-08 17:07:44 +0530

    Add ASPCoreDatabaseFirst

---
+++ ASPCoreDatabaseFirst/Models/AspcoreContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreDatabaseFirst.Models;
+
+public partial class AspcoreContext : DbContext
+{
+    public AspcoreContext()
+    {
+    }
+
+    public AspcoreContext(DbContextOptions<AspcoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=DREAM\\SQLEXPRESS;Database=ASPCore;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07CDCA600B");
+
+            entity.Property(e => e.Active)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Gender).HasMaxLength(10);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Position).HasMaxLength(100);
+            entity.Property(e => e.Salary).HasColumnType("decimal(18, 2)");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Gender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ASPCoreDatabaseFirst/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPCoreDatabaseFirst.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public string Position { get; set; } = null!;
+
+    //[Range(1000, int.MaxValue, ErrorMessage = "Salary must be at least 1000.")]
+    public decimal Salary { get; set; }
+
+    public string? Active { get; set; }
+}
